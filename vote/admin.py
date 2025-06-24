687dd6e47cab4182e6af5ce7e883cff261a6eaae
arafat2020 <arafatmannan2019@gmail.com>
2025-06-24 06:06:11 +0600

    vote initaited

--- vote/admin.py
+++ vote/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Vote
 
-admin.site.register(Post)
+admin.site.register(Vote)
 
 # Register your models here.
--- wasabi/urls.py
+++ wasabi/urls.py
@@ -38,5 +38,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('user.urls')),
     path('api/post/', include('post.urls')),
+    path('api/vote/', include('vote.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
